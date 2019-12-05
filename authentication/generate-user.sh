8e03f1bcfcaccf58f6c5b3bf646b3c3fddf2e818
Joerg Mueller <joerg.mueller@innoq.com>
2019-12-05 17:52:43 +0100

    WIP Authentication User.

---
+++ authentication/generate-user.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Creating a directory for the generated files
+AUTH_DIR=auth-data
+mkdir ${AUTH_DIR}
+
+# Creating a key using openssl
+openssl genrsa -out ${AUTH_DIR}/user-key.pem 2048
+
+# Creating a certificate sign request
+openssl req -new -key ${AUTH_DIR}/user-key.pem \
+    -subj "/CN=demo_user" \
+    -out ${AUTH_DIR}/user.csr \
+
+# Signing and generating the certificate using the minikube key
+openssl x509 -req -in ${AUTH_DIR}/user.csr -CA ~/.minikube/ca.crt \
+    -CAkey ~/.minikube/ca.key -CAcreateserial \
+    -out ${AUTH_DIR}/user.crt -days 500
+
+# Set the KUBECONFIG variable and create an empty file it wil be filled by the following commands 
+KUBECONFIG=${AUTH_DIR}/config
+touch ${AUTH_DIR}/config
+
+# Setting details of the config
+kubectl config set-cluster minikube --certificate-authority=${HOME}/.minikube/ca.crt --embed-certs=true --server=https://$(minikube ip):8443 
+kubectl config set-credentials demo_user --client-key=${AUTH_DIR}/user-key.pem --client-certificate=${AUTH_DIR}/user.crt --embed-certs=true 
+kubectl config set-context minikube_demo_user --user demo_user --cluster minikube 
+kubectl config use-context minikube_demo_user 
---
+++ pod_security_policies/README.md
@@ -0,0 +1,20 @@
+<!--
+Beispiel Yaml mit priviledged Container
+    - Was kann man hier demonstrieren?
+    - Einfach ein Shell Container in dem ich manuell ein Kommando aufrufe?
+
+PSP, die priviledged unterbindet
+
+Zeigen, dass der Pod noch läuft:
+    - Das ist wichtige Info. PSPs werden nicht auf vorhandene Pods angewendet
+
+Pod löschen
+Pod neu starten -> Zeigen, dass das nicht mehr geht
+
+
+META:
+
+PSP auf default namespace/service account anwenden
+Nur priviledged demonstrieren. Restliche Policies im Vortrag.
+-->
+
